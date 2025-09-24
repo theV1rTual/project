04535bf46a0d01cc16e50f44416340e2671f0457
theV1rTual <arystandev@gmail.com>
2025-09-24 20:09:20 +0500

    FIX: added envs

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,14 +1,15 @@
 import {MongoClient } from 'mongodb'
-
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const url = process.env.MONGO_URL;
 if (!url) {
     throw new Error('URL is not found')
 }
 const client = new MongoClient(url)
-
-export const blogsCollection = client.db('project2').collection('blogs');
-export const postsCollection = client.db('project2').collection('posts');
+//
+// export const blogsCollection = client.db('project2').collection('blogs');
+// export const postsCollection = client.db('project2').collection('posts');
 
 export const runDb = async () => {
     try {
