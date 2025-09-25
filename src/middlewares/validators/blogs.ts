369f56b9517c7d59deb841b2259889f9e4dd5388
theV1rTual <arystandev@gmail.com>
2025-09-25 17:40:00 +0500

    FIX: fixed validation

--- src/middlewares/validators/blogs.ts
+++ src/middlewares/validators/blogs.ts
@@ -4,16 +4,19 @@ const WEBSITE_RE = /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]
 
 export const blogCreateValidation = [
     body('name')
+        .exists().withMessage('name is required')
         .trim()
         .isString().withMessage('name must be a string')
         .isLength({max: 15}).withMessage('name length cannot be more than 15'),
 
     body('description')
+        .exists().withMessage('description is required')
         .trim()
         .isString().withMessage('description must be a string')
         .isLength({max: 500}).withMessage('description length cannot be more than 500'),
 
     body('websiteUrl')
+        .exists().withMessage('websiteUrl is required')
         .trim()
         .isString().withMessage('websiteUrl must be a string')
         .isLength({max: 100}).withMessage('websiteUrl length cannot be more than 100')
--- src/middlewares/validators/posts.ts
+++ src/middlewares/validators/posts.ts
@@ -2,21 +2,25 @@ import {body} from "express-validator";
 
 export const postCreateValidation = [
     body('title')
+        .exists().withMessage('title is required')
         .trim()
         .isString().withMessage('title must be a string')
         .isLength({max: 30}).withMessage('title length cannot be more than 30'),
 
     body('shortDescription')
+        .exists().withMessage('shortDescription is required')
         .trim()
         .isString().withMessage('shortDescription must be a string')
         .isLength({max: 100}).withMessage('shortDescription length cannot be more than 30'),
 
     body('content')
+        .exists().withMessage('content is required')
         .trim()
         .isString().withMessage('content must be a string')
         .isLength({max: 1000}).withMessage('content length cannot be more than 30'),
 
     body('blogId')
+        .exists().withMessage('blogId is required')
         .trim()
         .isString().withMessage('blogId must be a string')
 
