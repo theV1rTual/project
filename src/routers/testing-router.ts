8a14d882c08ff094a56dda9ce4dac801e759d3f8
theV1rTual <arystandev@gmail.com>
2025-09-25 13:05:17 +0500

    FIX: added test get method

--- src/routers/testing-router.ts
+++ src/routers/testing-router.ts
@@ -4,6 +4,10 @@ import {blogsCollection} from "../repositories/db";
 
 export const testingRouter = Router({})
 
+testingRouter.get('/', (_req: Request, res: Response) => {
+    res.json({ ok: true });
+});
+
 testingRouter.delete('/all-data', async (req: Request, res: Response) => {
     try {
         await blogsCollection.deleteMany({})
