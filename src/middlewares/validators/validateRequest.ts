f71afdd064434d31c23965917e3e98541357b7d8
theV1rTual <arystandev@gmail.com>
2025-09-25 17:35:59 +0500

    FIX: fixed validation

--- src/middlewares/validators/validateRequest.ts
+++ src/middlewares/validators/validateRequest.ts
@@ -6,10 +6,10 @@ export const validateRequest = (req: Request, res: Response, next: NextFunction)
     const result = validationResult(req);
     if (result.isEmpty()) return next();
 
-    const errorMessages = result.array({onlyFirstError: true}).map(err => ({
+    const errorsMessages = result.array({onlyFirstError: true}).map(err => ({
         message: err.msg,
         field: err.type === 'field' ? err.path : 'unknown'
     }))
 
-    return res.status(400).json({errorMessages})
+    return res.status(400).json({errorsMessages})
 }
